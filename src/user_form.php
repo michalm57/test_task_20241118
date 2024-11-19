d4c83328479d82e97a3951dce118370895e3e24f
michalm57 <mroczekmichal57@gmail.com>
2024-11-19 16:06:22 +0100

    add counting functions

--- src/user_form.php
+++ src/user_form.php
@@ -2,19 +2,23 @@
 
 namespace Src;
 
-require __DIR__ . '/database.php';
+require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/user_repository.php';
 
 use Src\Database;
+use Src\UserRepository;
 use PDOException;
 
 class UserForm
 {
     protected $db;
+    protected $userRepo;
 
     public function __construct()
     {
         $database = new Database();
         $this->db = $database->connect();
+        $this->userRepo = new UserRepository($this->db);
     }
 
     private function sanitize_data($data)
@@ -42,10 +46,16 @@ class UserForm
 
         $data = $this->map_data($data);
 
-        if($data = $this->save_form_to_db($data)) {
-            return $this->json_response(200, $data, 'Data saved!');
+        if ($this->save_form_to_db($data)) {
+            $surname_count = $this->userRepo->fetch_surname_counter('Kowalski');
+            $domain_count = $this->userRepo->fetch_email_domain_counter('gmail.com');
+
+            return $this->json_response(200, [
+                'surname_count' => $surname_count,
+                'domain_count' => $domain_count
+            ], 'Data saved!');
         } else {
-            return $this->json_response(200, $data, 'Error!');
+            return $this->json_response(200, [], 'Error!');
         }
     }
 
@@ -56,13 +66,8 @@ class UserForm
                       VALUES (:name, :surname, :email, :phone, :choose, :client_no, :agreement1, :agreement2, :userinfo)";
             $exec = $this->db->prepare($query);
             $exec->execute($data);
-            $lastInsertId = $this->db->lastInsertId();
-            $query = "SELECT * FROM zadanie WHERE id = :id";
-            $exec = $this->db->prepare($query);
-            $exec->bindParam(':id', $lastInsertId, PDO::PARAM_INT);
-            $exec->execute();
-    
-            return $exec->fetch(PDO::FETCH_ASSOC);
+
+            return true;
         } catch (PDOException $e) {
             return false;
         }
@@ -77,32 +82,21 @@ class UserForm
             'phone'     => $data['phone'],
             'choose'    => $data['choose'],
             'client_no' => $data['client_no'],
-            'agreement1'=> $data['agreement'],
-            'agreement2'=> $data['agreement'],
+            'agreement1' => $data['agreement'],
+            'agreement2' => $data['agreement'],
             'userinfo'  => $data['userinfo'],
         ];
-    }
-
-    public function counter($surname)
-    {
-        try {
-            $db = Database::getConnection();
-            $stmt = $db->prepare("SELECT id_form FROM `zadanie` WHERE surname = :surname");
-            $stmt->execute(['surname' => $surname]);
-
-            return $stmt->rowCount();
-        } catch (PDOException $e) {
-            return 0;
-        }
     }
 
     private function is_form_valid($data)
     {
         $errors = [];
-        $requiredFields = ['name', 'surname', 'email', 'client_no', 'choose', 'agreement1', 'agreement2'];
+        $requiredFields = ['name', 'surname', 'email', 'phone', 'client_no', 'choose', 'agreement1', 'agreement2'];
+        $data['account'] = str_replace(' ', '', $data['account']);
+
         foreach ($requiredFields as $field) {
             if (empty($data[$field])) {
-                $errors[] = "Pole {$field} jest wymagane.";
+                $errors[] = "Pole {$this->map_field_name($field)} jest wymagane.";
             }
         }
         if (!empty($data['client_no']) && !preg_match('/^\d{3}[0-9]{3}-[A-Z]{5}$/', $data['client_no'])) {
@@ -111,8 +105,8 @@ class UserForm
         if (!empty($data['choose']) && $data['choose'] === '1') {
             if (empty($data['account'])) {
                 $errors[] = "Pole Numer konta jest wymagane przy wyborze 1.";
-            } elseif (!preg_match('/^\d{26}$/', $data['account'])) {
-                $errors[] = "Numer konta musi być polskim numerem (26 cyfr).";
+            } elseif (!preg_match('/^PL\d{26}$/', $data['account'])) {
+                $errors[] = "Numer konta musi być polskim numerem (PL + 26 cyfr).";
             }
         }
         if (!empty($data['phone']) && !preg_match('/^\d{9}$/', $data['phone'])) {
@@ -127,8 +121,23 @@ class UserForm
         if (empty($data['agreement2']) || $data['agreement2'] !== 'on') {
             $errors[] = "Musisz zaakceptować Oświadczenie 2.";
         }
-    
+
         return empty($errors) ? ['valid' => true, 'data' => []] : ['valid' => false, 'data' => $errors];
+    }
+
+    private function map_field_name($fieldName)
+    {
+        return match ($fieldName) {
+            'name' => 'Imię',
+            'surname' => 'Nazwisko',
+            'phone' => 'Numer telefonu komórkowego',
+            'email' => 'Adres e-mail',
+            'client_no' => 'Numer klienta',
+            'agreement1' => 'Oświadczenie 1',
+            'agreement2' => 'Oświadczenie 2',
+            'choose' => 'Wybór',
+            default => 'Inne'
+        };
     }
 
     private function json_response($code = 200, $data = [], $message = null)
@@ -144,13 +153,7 @@ class UserForm
     }
 }
 
-function get_surname_counter($surname)
-{
-    $frm = new UserForm;
-    return (int) $frm->counter($surname);
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {    
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $frm = new UserForm();
     $result = $frm->save_form($_POST);
 
--- src/user_repository.php
+++ src/user_repository.php
@@ -17,7 +17,7 @@ class UserRepository
         $this->db = $database->connect();
     }
 
-    public function fetchPaginatedData($params)
+    public function fetch_paginated_data($params)
     {
         $columns = [
             'name',
@@ -29,40 +29,40 @@ class UserRepository
             'agreement1',
             'userinfo',
         ];
-    
+
         $start = $params['start'] ?? 0;
         $length = $params['length'] ?? 10;
-    
+
         $orderColumnIndex = $params['order'][0]['column'] ?? 0;
         $orderColumn = $columns[$orderColumnIndex] ?? 'id';
         $orderDir = isset($params['order'][0]['dir']) && $params['order'][0]['dir'] === 'desc' ? 'DESC' : 'ASC';
-    
+
         $searchValue = $params['search']['value'] ?? '';
-    
+
         $query = "SELECT * FROM zadanie";
         $queryParams = [];
-    
+
         if (!empty($searchValue)) {
             $query .= " WHERE name LIKE :search OR surname LIKE :search OR email LIKE :search";
             $queryParams['search'] = '%' . $searchValue . '%';
         }
-    
+
         $query .= " ORDER BY $orderColumn $orderDir LIMIT :start, :length";
-    
+
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':start', (int)$start, PDO::PARAM_INT);
         $stmt->bindValue(':length', (int)$length, PDO::PARAM_INT);
-    
+
         foreach ($queryParams as $key => $value) {
             $stmt->bindValue(":$key", $value);
         }
-    
+
         $stmt->execute();
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+
         $totalQuery = "SELECT COUNT(*) FROM zadanie";
         $totalRecords = $this->db->query($totalQuery)->fetchColumn();
-    
+
         $filteredQuery = "SELECT COUNT(*) FROM zadanie";
         if (!empty($searchValue)) {
             $filteredQuery .= " WHERE name LIKE :search OR surname LIKE :search OR email LIKE :search";
@@ -73,12 +73,36 @@ class UserRepository
         }
         $filteredStmt->execute();
         $filteredRecords = $filteredStmt->fetchColumn();
-    
+
         return [
             'draw' => $params['draw'] ?? 1,
             'recordsTotal' => (int)$totalRecords,
             'recordsFiltered' => (int)$filteredRecords,
             'data' => $data,
         ];
-    }    
+    }
+
+    public function fetch_surname_counter($surname)
+    {
+        try {
+            $stmt = $this->db->prepare("SELECT id_form FROM `zadanie` WHERE surname = :surname");
+            $stmt->execute(['surname' => $surname]);
+
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            return 0;
+        }
+    }
+
+    public function fetch_email_domain_counter($domain)
+    {
+        try {
+            $stmt = $this->db->prepare("SELECT id_form FROM `zadanie` WHERE email LIKE :domain");
+            $stmt->execute(['domain' => '%@' . $domain]);
+
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            return 0;
+        }
+    }
 }
