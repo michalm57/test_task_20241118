2cc846f5ecd328fc8bec6731b585e32f3a7e74d4
michalm57 <mroczekmichal57@gmail.com>
2024-11-20 17:09:12 +0100

    database connection as singleton

--- src/database.php
+++ src/database.php
@@ -4,20 +4,40 @@ namespace Src;
 
 class Database
 {
-    public function connect()
+    private static ?self $instance = null;
+    private static ?\PDO $connection = null;
+
+    private function __construct()
     {
         $config = require __DIR__ . '/../config/db_connection.php';
 
         try {
-            $db = new \PDO(
+            self::$connection = new \PDO(
                 "mysql:host={$config['db_host']};dbname={$config['db_name']}",
                 $config['db_username'],
                 $config['db_password']
             );
-            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            return $db;
+            self::$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
+    }
+
+    public static function get_instance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public static function get_connection(): \PDO
+    {
+        if (self::$connection === null) {
+            self::get_instance();
+        }
+
+        return self::$connection;
     }
 }
